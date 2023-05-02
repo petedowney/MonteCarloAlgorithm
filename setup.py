186b1d9f6712d79ac8999a608141c44f3526242c
petedowney <peter.d.downey@gmail.com>
2023-05-02 12:16:38 -0400

    alsdkjf

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='MonteCarloAlgorithm',
+    version='',
+    packages=['MonteCarloAlgorithm', 'MonteCarloAlgorithm.tests', 'MonteCarloAlgorithm.bitstring',
+              'MonteCarloAlgorithm.metropolis', 'MonteCarloAlgorithm.isingHamiltonian'],
+    url='',
+    license='',
+    author='peter',
+    author_email='',
+    description=''
+)
